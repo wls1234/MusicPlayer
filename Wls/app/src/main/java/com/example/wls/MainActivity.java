c577d9b2db0cd8e9cd46e7caa2411827020f1243
wls1234 <1944800323@qq.com>
2019-06-19 17:16:42 +0800

    qqq

---
+++ Wls/app/src/main/java/com/example/wls/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.wls;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.example.wls.adapter.MusicPagerAdapter;
+import com.example.wls.fragment.MyMusicFragment;
+import com.example.wls.fragment.RecFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//实现OnClickListener的接口
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //定义activity_main.xml的控件对象
+    private TextView logicTv;
+    private TextView onlineTv;
+    private ViewPager viewPager;
+    //将Fragment放入List集合中，存放fragment对象
+    private List<Fragment> fragmentList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //绑定id
+        bangdingID();
+        //设置监听
+        jianting();
+        //创建fragment对象
+        MyMusicFragment myMusicFragment= new MyMusicFragment();
+        RecFragment recFragment = new RecFragment();
+        //将fragment对象添加到fragmentList中
+        fragmentList.add(myMusicFragment);
+        fragmentList.add(recFragment);
+        //通过MusicPagerAdapter类创建musicPagerAdapter的适配器，MusicPagerAdapter类的创建方法
+        MusicPagerAdapter musicPagerAdapter = new MusicPagerAdapter(getSupportFragmentManager(), fragmentList);
+        //viewPager绑定适配器
+        viewPager.setAdapter(musicPagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        logicTv.setTextColor(getResources().getColor(R.color.white));
+                        onlineTv.setTextColor(getResources().getColor(R.color.white_60P));
+                        break;
+                    case 1:
+                        onlineTv.setTextColor(getResources().getColor(R.color.white));
+                        logicTv.setTextColor(getResources().getColor(R.color.white_60P));
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+    private void jianting() {
+        logicTv.setOnClickListener(this);
+        onlineTv.setOnClickListener(this);
+    }
+
+    private void bangdingID() {
+        logicTv = findViewById(R.id.main_logic_tv);
+        onlineTv = findViewById(R.id.main_online_tv);
+        viewPager = findViewById(R.id.main_vp);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_logic_tv:
+                //实现点击TextView切换fragment
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.main_online_tv:
+                viewPager.setCurrentItem(1);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+//    @Override
+//    public  boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+//            final View view = LayoutInflater.from(this).inflate(R.layout.dialog1_layout, null);
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setTitle("退出提示");
+//            builder.setView(view);
+//            builder.setPositiveButton("是", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    finish();
+//                }
+//            });
+//            builder.setNegativeButton("否", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialogInterface, int i) {
+//                    finish();
+//                }
+//            });
+//            return super.onKeyDown(keyCode, event);
+//        }
+//    }
+}
+
+
---
+++ Wls/app/src/main/java/com/example/wls/utils/Common.java
@@ -0,0 +1,11 @@
+package com.example.wls.utils;
+
+import com.example.wls.entity.Music;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+
+    public static List<Music> musicList = new ArrayList<>();
+}
